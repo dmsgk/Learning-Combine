b9a4a342f7c04fab4b9fadf9bbe10772b88a90e5
johyeon yoon <yds03080@naver.com>
2021-08-21 15:02:18 +0900

    Initial setting

---
+++ CombinePractice1/CombinePractice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombinePractice1
+//
+//  Created by Johyeon Yoon on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
